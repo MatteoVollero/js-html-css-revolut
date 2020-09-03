042ba1b5ea44246dfda9622036c092262645f138
MatteoVollero <m.vollero120796@gmail.com>
2020-09-03 18:39:53 +0200

    funzione click non allineata

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-
+$("#aiuto").click(
+  function(){
+    if($(".off").hasClass("off")){
+      console.log("Ha la classe off");
+      $("#menu").removeClass("off");
+    }else{
+      console.log("Non ha la class off");
+      $("#menu").addClass("off");
+    }
+  }
+);
