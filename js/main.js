d7d521b223689358b39f50e5a4a258bf6d79544e
MatteoVollero <m.vollero120796@gmail.com>
2020-09-06 17:37:51 +0200

    menu a comparsa

--- js/main.js
+++ js/main.js
@@ -1,11 +1,72 @@
-$("#aiuto").click(
+$(document).ready(
   function(){
-    if($(".off").hasClass("off")){
-      console.log("Ha la classe off");
-      $("#menu").removeClass("off");
-    }else{
-      console.log("Non ha la class off");
-      $("#menu").addClass("off");
-    }
-  }
-);
+
+
+    $("#tariffazione").mouseenter(
+      function(){
+        console.log("mouse entra");
+        $("#tariffazione-menu").removeClass("off");
+      });
+
+
+    $("#tariffazione").mouseleave(
+      function(){
+        console.log("mouse esce");
+        $("#tariffazione-menu").addClass("off");
+      });
+
+
+      $("#conti").mouseenter(
+        function(){
+          console.log("mouse entra");
+          $("#conti-menu").removeClass("off");
+        });
+
+
+      $("#conti").mouseleave(
+        function(){
+          console.log("mouse esce");
+          $("#conti-menu").addClass("off");
+        });
+
+
+      $("#prodotti").mouseenter(
+        function(){
+          console.log("mouse entra");
+          $("#prodotti-menu").removeClass("off");
+        });
+
+
+      $("#prodotti").mouseleave(
+        function(){
+          console.log("mouse esce");
+          $("#prodotti-menu").addClass("off");
+        });
+
+        $("#azienda").mouseenter(
+          function(){
+            console.log("mouse entra");
+            $("#azienda-menu").removeClass("off");
+          });
+
+
+        $("#azienda").mouseleave(
+          function(){
+            console.log("mouse esce");
+            $("#azienda-menu").addClass("off");
+          });
+
+          $("#aiuto").mouseenter(
+            function(){
+              console.log("mouse entra");
+              $("#aiuto-menu").removeClass("off");
+            });
+
+
+          $("#aiuto").mouseleave(
+            function(){
+              console.log("mouse esce");
+              $("#aiuto-menu").addClass("off");
+            });
+
+  });
